665a8b270b4b84cddcc2cd66b9574a53b51e0d08
James Gordon <gordon.je@gmail.com>
2018-03-14 10:58:46 -0500

    Require login (temporarily) to all views

--- factchecker/views.py
+++ factchecker/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.views.decorators.http import require_safe
@@ -5,6 +6,7 @@ from .models import ClaimReview
 
 
 @require_safe
+@login_required
 def index(request):
     claim_review_list = ClaimReview.objects.filter(
         published_on__lte=timezone.now()
@@ -20,6 +22,7 @@ def index(request):
 
 
 @require_safe
+@login_required
 def detail(request, claim_review_id):
     context = {
         'claim_review': get_object_or_404(ClaimReview, pk=claim_review_id)
