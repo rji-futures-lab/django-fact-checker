5110fcf6c4a0bba54bbe33658a401c13121883aa
James Gordon <gordon.je@gmail.com>
2018-03-19 17:28:07 -0500

    DRAFT: Add emoji elements to page with random gender, skin tone

--- factchecker/views.py
+++ factchecker/views.py
@@ -1,9 +1,37 @@
+from random import randint
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.views.decorators.http import require_safe
 from .models import ClaimReview
 
+
+def get_random_skin_tone():
+    skin_tones = ('🏻', '🏼', '🏽', '🏾', '🏿',)
+    rand_int = randint(0, 4)
+    return skin_tones[rand_int]
+
+def get_random_gender():
+    genders = ('♀', '♂')
+    rand_int = randint(0, 1)
+    return genders[rand_int]
+
+
+def randomize_emoji(emoji_char):
+    zero_width_joiner = chr(int('U+200D'[2:], 16))
+    variation_select_char = chr(int('U+FE0F'[2:], 16))
+
+    combine = ''.join([
+        emoji_char,
+        get_random_skin_tone(),
+        zero_width_joiner,
+        get_random_gender(),
+        variation_select_char
+    ])
+
+    print(combine)
+
+    return combine
 
 @require_safe
 @login_required
@@ -14,6 +42,8 @@ def index(request):
 
     context = {
         'claim_review_list': claim_review_list,
+        'skin_tone': get_random_skin_tone(),
+        'gender': get_random_gender(),
     }
 
     return render(request, 'factchecker/index.html', context)
@@ -23,7 +53,9 @@ def index(request):
 @login_required
 def detail(request, claim_review_id):
     context = {
-        'claim_review': get_object_or_404(ClaimReview, pk=claim_review_id)
+        'claim_review': get_object_or_404(ClaimReview, pk=claim_review_id),
+        'skin_tone': get_random_skin_tone(),
+        'gender': get_random_gender(),
     }
     
     return render(request, 'factchecker/detail.html', context)
